f8a8e1b7f64f80bfc1327663baafea85f2802475
cbonamin <cbonamin@gmail.com>
2020-04-06 16:19:47 -0300

    Add Cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -44,6 +44,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
@@ -51,4 +56,5 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
 end
